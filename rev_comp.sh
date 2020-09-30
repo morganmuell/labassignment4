fd3a572ff9991e3c378d20abf68b19d6e7f66daa
Morgan R. Muell <morganrmuell@gmail.com>
2020-09-30 16:59:13 -0500

    Morgan added a one-liner for reversing the sequence rather than using a loop and it might work but we'll find out.

---
+++ rev_comp.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+#initialize variable
+reverse=""
+
+#read in data
+name=`grep ">" $1`
+sequence=`grep -v ">" $1`
+
+echo "Name: $name"
+
+#calculate length of sequence
+len=${#sequence}
+
+echo "Length: $len"
+
+#loop through sequence in reverse
+reverse=`grep -v ">" $1 | rev`
+
+echo "$sequence
+$reverse
+"
+
+#complement sequence
+rc=`echo $reverse | tr 'atcg' 'tagc'`
+
+echo "$rc
+"
+
+echo $name >$1.rc.txt
+echo $rc >>$1.rc.txt
